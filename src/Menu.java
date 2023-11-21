f764aebbec0f27c6f2ae78b1c6dd68fa9db1c2c8
bjli0001 <bjli0001@stud.kea.dk>
2023-11-21 10:51:20 +0100

    .

---
+++ src/Menu.java
@@ -0,0 +1,62 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Menu {
+    static int op;
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+    static void menu(String[] list){
+        for(int i=0; i<list.length; i++){
+
+            System.out.println("Tast "+(i+1)+": "+list[i]);
+
+        }
+        System.out.println("Vælg menupunkt [1,"+list.length+"]:");
+        op = inInt(list.length);
+    }
+
+    static int inInt(int size) {
+
+        boolean OK=false;
+        int num=0;
+        do {
+            try {
+                num= input.nextInt();
+                input.nextLine();
+                if (num<=size) {
+                    OK = true;
+                }
+                else {
+                    System.out.println("Ulovligt input. Prøv igen");
+                    input.nextLine();
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Ulovligt input. Prøv igen");
+                input.nextLine();
+            }
+        } while(!OK);
+        return num;
+    }
+
+//    static String inTlf() {
+//        boolean cont = true;
+//        String tlf = "";
+//        while (cont) {
+//            System.out.println("Indtast telefon nr.");
+//            tlf = input.nextLine();
+//
+//            if (tlf.length() == 8) {
+//                try {
+//                    Integer.parseInt(tlf);
+//                    cont = false;
+//                } catch (Exception e) {
+//                    System.out.println("Ugyldigt telefon nr.");
+//                }
+//            } else System.out.println("Ugyldigt telefon nr.");
+//        }
+//        return tlf;
+//    }
+}
---
+++ src/ToFile.java
@@ -0,0 +1,33 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+public class ToFile {
+
+    public static void saveList(ArrayList<String[]> dataLines,String s) throws IOException {
+        FileWriter fil = new FileWriter(s);
+        PrintWriter ud = new PrintWriter(fil);
+
+        for (String[] sa: dataLines){
+
+            ud.println(String.join(",",sa));
+        }
+        fil.close();
+    }
+    public static void saveCustomer(ArrayList<Customer> dataLines) throws IOException {
+        FileWriter fil = new FileWriter("customers.txt");
+        PrintWriter ud = new PrintWriter(fil);
+
+        for (Customer k: dataLines) {
+            ArrayList<String> bookArr = new ArrayList<>();
+            for (String[] s: k.bookings){
+
+                bookArr.add(String.join(".",s));
+
+            }
+            ud.println(k.tlfnr +","+ k.name +","+ String.join(";",bookArr));
+        }
+        fil.close();
+    }
+}
